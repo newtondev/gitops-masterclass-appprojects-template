35d6e5d6aba71b78e47f04f08a5a33fd8aa3915a
Christian Hernandez <christian@chernand.io>
2025-04-15 15:39:11 -0700

    helm install argocd
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -2,8 +2,15 @@
 
 echo "post-create start" >> ~/.status.log
 
-# this runs in background after UI is available
-
+# Install the K3D cluster
 k3d cluster create --config .devcontainer/manifests/k3d-dev.yaml --wait | tee -a ~/.status.log
+
+# Install Argo CD using Helm
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argo/argo-cd --namespace argocd --create-namespace \
+    --set server.service.type="NodePort" \
+    --set server.service.nodePortHttp=30272
+
 
 echo "post-create complete" >> ~/.status.log
--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -7,11 +7,6 @@ echo "post-start start" >> ~/status.log
 # Ensure kubeconfig is set up. 
 k3d kubeconfig merge dev --kubeconfig-merge-default
 
-# Install Argo CD using Helm
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
-helm install argocd argo/argo-cd --namespace argocd --create-namespace --values ../manifests/argocd-values.yaml
-
 bash .devcontainer/scripts/update-repo-for-workshop.sh
 
 # Best effort env load
