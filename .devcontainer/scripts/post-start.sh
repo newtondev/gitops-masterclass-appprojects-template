69b869b744ca171b406f98f3459e69d717f5d12e
Christian Hernandez <christian@chernand.io>
2025-04-15 15:25:29 -0700

    changed nodePortHttp
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -7,6 +7,11 @@ echo "post-start start" >> ~/status.log
 # Ensure kubeconfig is set up. 
 k3d kubeconfig merge dev --kubeconfig-merge-default
 
+# Install Argo CD using Helm
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argo/argo-cd --namespace argocd --create-namespace --values ../manifests/argocd-values.yaml
+
 bash .devcontainer/scripts/update-repo-for-workshop.sh
 
 # Best effort env load
