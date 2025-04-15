7ad79c6719f5f60c5600133f876528d858b6abec
Christian Hernandez <christian@chernand.io>
2025-04-15 16:23:35 -0700

    login in as argocd and changing pass
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -7,7 +7,15 @@ echo "post-start start" >> ~/status.log
 # Ensure kubeconfig is set up. 
 k3d kubeconfig merge dev --kubeconfig-merge-default
 
+# Update the repo for the workshop
 bash .devcontainer/scripts/update-repo-for-workshop.sh
+
+# Update Argo CD Admin Password
+argopass=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
+argouri="localhost:30272"
+argonewpass="password"
+argocd login --insecure --username ${argouser:=admin} --password ${argopass} --grpc-web ${argouri}
+argocd account --insecure update-password --insecure --current-password ${argopass} --new-password ${argonewpass}
 
 # Best effort env load
 source ~/.bashrc
