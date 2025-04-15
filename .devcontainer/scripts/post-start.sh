c25e7452f76b9a02cae83c08c285886c5f71c0fb
Christian Hernandez <christian@chernand.io>
2025-04-15 16:45:58 -0700

    wait for Argo CD to be ready
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -10,13 +10,15 @@ k3d kubeconfig merge dev --kubeconfig-merge-default | tee -a ~/.status.log
 # Update the repo for the workshop
 bash .devcontainer/scripts/update-repo-for-workshop.sh | tee -a  ~/.status.log 
 
+# Wit for Argo CD to be ready
+kubectl rollout status -n argocd sts/argocd-application-controller | tee -a  ~/.status.log
+
 # Update Argo CD Admin Password
 argopass=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
 argouri="localhost:30272"
 argonewpass="password"
 argocd login --insecure --username ${argouser:=admin} --password ${argopass} --grpc-web ${argouri} | tee -a  ~/.status.log 
 argocd account --insecure update-password --insecure --current-password ${argopass} --new-password ${argonewpass} | tee -a  ~/.status.log 
-argocd login --insecure --username ${argouser:=admin} --password ${argonewpass} --grpc-web ${argouri} | tee -a  ~/.status.log 
 
 # Best effort env load
 source ~/.bashrc
