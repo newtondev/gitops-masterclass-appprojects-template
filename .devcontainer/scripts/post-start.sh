0bd090a3377456adca219fbad2a6e7c2c6c1e7f1
Christian Hernandez <christian@chernand.io>
2025-04-15 17:42:52 -0700

    patching URL
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -18,7 +18,7 @@ counter=0
 until [[ $(curl -s -o /dev/null -w "%{http_code}" localhost:30272) -eq 307 ]]
 do
     echo "Waiting for Argo CD endpoint to be ready..." | tee -a  ~/.status.log
-    sleep 5
+    sleep 3
     counter=$((counter+1))
     if [[ $counter -gt 60 ]]; then
         echo "Port not available for Argo CD CLI" | tee -a  ~/.status.log
@@ -32,6 +32,9 @@ argouri="localhost:30272"
 argonewpass="password"
 argocd login --insecure --username ${argouser:=admin} --password ${argopass} --grpc-web ${argouri} | tee -a  ~/.status.log 
 argocd account --insecure update-password --insecure --current-password ${argopass} --new-password ${argonewpass} | tee -a  ~/.status.log 
+
+# Patch URL value. Probably can do this via helm in the "post-create.sh" script. PRs are welcome
+kubectl patch cm/argocd-cm -n argocd --type=json  -p="[{\"op\": \"replace\", \"path\": \"/data/url\", \"value\":\"https://${CODESPACE_NAME}-30272.app.github.dev\"}]" | tee -a  ~/.status.log
 
 # Best effort env load
 source ~/.bashrc
